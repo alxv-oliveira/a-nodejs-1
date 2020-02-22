1980a9b371b0319bbe9c49b5df1ae57cccbe86a4
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-22 16:03:43 -0300

    02.Express e templates com Marko

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -1,28 +1,5 @@
-const servidor  = http.createServer(function (req, resp) {
-    let html = '';
+const app = require('./src/config/custom-express');
 
-    if (req.url == '/') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-    `;
-    } else if (req.url == '/livros') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Listagem de livros </h1>
-            </body> 
-        </html>
-        `;
-    } 
-    resp.end(html);
-}
+app.listen(3000, function() {
+    console.log(`Servidor rodando na porta 3000`);
+});
