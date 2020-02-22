8d6fb8d5512ab5215e43da053d2068610c446a1d
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-22 17:39:56 -0300

    04.Manipulando a requisição com Middlewares

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -1,4 +1,4 @@
-const LivroDao = require('../infra/livro-dao');
+const LivroDao = require('../infra/LivroDao');
 const db = require('../../config/database');
 
 module.exports = (app) => {
@@ -28,15 +28,17 @@ module.exports = (app) => {
                     }
                 ))
                 .catch(erro => console.log(erro));
-        // livroDao.lista(function(erro, resultados) {
+    });
 
-        //     resp.marko(
-        //         require('../views/livros/lista/lista.marko'),
-        //         {
-        //             livros: resultados
-        //         }
-        //     );
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'));
+    });
 
-        // });
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
     });
 };
